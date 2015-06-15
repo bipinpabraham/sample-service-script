92c2fad16b80cd8d7087617d9545513392e4ac41
Yuriy Boychenko <yuboychenko@gmail.com>
2015-06-15 20:05:41 +0300

    Update new-service.sh

--- new-service.sh
+++ new-service.sh
@@ -3,8 +3,7 @@
 SERVICE_FILE=$(tempfile)
 
 echo "--- Download template ---"
-#wget -q -O "$SERVICE_FILE" 'https://raw.githubusercontent.com/spheresh/sample-service-script/master/service.sh' || { echo 'ERROR: Could not retreive service.sh from github'; exit 1;}
-cp service.sh $SERVICE_FILE
+wget -q -O "$SERVICE_FILE" 'https://raw.githubusercontent.com/spheresh/sample-service-script/master/service.sh' || { echo 'ERROR: Could not retreive service.sh from github'; exit 1;}
 
 chmod +x "$SERVICE_FILE"
 echo ""
